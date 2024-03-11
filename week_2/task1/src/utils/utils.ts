cf1afd0455902b561e513fe2374a2dbe5baf085b
Vlad Maslianko <masliankovladik@gmail.ocm>
2024-03-11 23:52:39 +0200

    Add try-catch block in utls funcs

--- week_2/task1/src/utils/utils.ts
+++ week_2/task1/src/utils/utils.ts
@@ -6,22 +6,33 @@ export const employeesFilename = './data/employees.json';
 export const holidaysFilename = './data/holidays.json';
 
 export function getEmployees(): Employee[] {
-  const rawData = fs.readFileSync(employeesFilename);
-  const jsonData: Employee[] = JSON.parse(rawData.toString());
-  return jsonData;
+  try {
+    const rawData = fs.readFileSync(employeesFilename, 'utf-8');
+    const jsonData: Employee[] = JSON.parse(rawData);
+    return jsonData;
+  } catch (error) {
+    console.error('Error reading employees data:', error);
+    return [];
+  }
 }
 
 export function getHolidayRequests(): HolidayRequest[] {
-  const rawData = fs.readFileSync(holidaysFilename);
-  const jsonData: HolidayRequest[] = JSON.parse(rawData.toString());
+  try {
+    const rawData = fs.readFileSync(holidaysFilename, 'utf-8');
+    const jsonData: HolidayRequest[] = JSON.parse(rawData);
 
-  jsonData.forEach(request => {
-    request.startDate = new Date(request.startDate);
-    request.endDate = new Date(request.endDate);
-  });
+    jsonData.forEach(request => {
+      request.startDate = new Date(request.startDate);
+      request.endDate = new Date(request.endDate);
+    });
 
-  return jsonData;
+    return jsonData;
+  } catch (error) {
+    console.error('Error reading holiday requests data:', error);
+    return [];
+  }
 }
+
 
 export function getNameById(id: number): string | undefined {
     const employees: Employee[] = getEmployees();
